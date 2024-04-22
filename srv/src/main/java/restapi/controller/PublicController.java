4e72cc54dfd4d12a8e01b8e15f7669b888d4e856
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-04-22 12:32:33 +0530

    rest

--- srv/src/main/java/restapi/controller/PublicController.java
+++ srv/src/main/java/restapi/controller/PublicController.java
@@ -2,9 +2,11 @@ package restapi.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.extern.slf4j.Slf4j;
 
+@RestController
 @RequestMapping("/public")
 @Slf4j
 public class PublicController
