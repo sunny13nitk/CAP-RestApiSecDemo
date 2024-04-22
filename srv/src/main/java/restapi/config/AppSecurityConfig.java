0924481f60515627dcf696ce781ed66c2e90f1bc
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-04-22 12:07:50 +0530

    @Order Add to override CAP security

--- srv/src/main/java/restapi/config/AppSecurityConfig.java
+++ srv/src/main/java/restapi/config/AppSecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.annotation.Order;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
