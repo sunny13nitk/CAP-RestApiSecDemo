025a186519d873f732ef53e7f6918a303b6231c3
Sunny Bhardwaj <sunny13nitk@gmail.com>
2024-05-15 09:28:43 +0530

    admin token

--- srv/src/main/java/restapi/controller/AuthController.java
+++ srv/src/main/java/restapi/controller/AuthController.java
@@ -307,6 +307,122 @@ public class AuthController
         return new ResponseEntity<>(bearer, HttpStatus.OK);
     }
 
+    @GetMapping("/adminToken")
+    public ResponseEntity<TY_BearerToken> getToken() throws IOException
+    {
+        TY_ApplicationDetails acCodeParams;
+        TY_BearerToken bearer = null;
+        CloseableHttpClient httpClient = null;
+
+        try
+        {
+            acCodeParams = CL_DestinationUtilities.getAccessCodeParams4OAuthDestination(desNameOAuthCred);
+
+            if (acCodeParams != null)
+            {
+                // Create an HTTP POST request to the token endpoint
+                String url = acCodeParams.getAuthUrl();
+
+                if (StringUtils.hasText(url) && StringUtils.hasText(acCodeParams.getClientId())
+                        && StringUtils.hasText(acCodeParams.getClientSecret()))
+                {
+
+                    TY_TokenRequestBody reqBody = new TY_TokenRequestBody(CL_DestinationUtilities.GC_ClientCredentials,
+                            acCodeParams.getClientId(), acCodeParams.getClientSecret());
+                    if (reqBody != null)
+                    {
+
+                        List<NameValuePair> formparams = new ArrayList<NameValuePair>();
+                        formparams.add(new BasicNameValuePair(CL_DestinationUtilities.GC_GrantType,
+                                CL_DestinationUtilities.GC_ClientCredentials));
+                        formparams.add(new BasicNameValuePair(CL_DestinationUtilities.GC_ClientID_Token,
+                                reqBody.getClient_id()));
+                        formparams.add(new BasicNameValuePair(CL_DestinationUtilities.GC_ClientSecret_Token,
+                                reqBody.getClient_secret()));
+
+                        httpClient = HttpClientBuilder.create().build();
+
+                        HttpPost httpPost = new HttpPost(url);
+
+                        // Set the request headers
+                        httpPost.addHeader("Content-Type", "application/x-www-form-urlencoded");
+                        httpPost.addHeader("Accept", "application/json");
+
+                        // Write the request body
+                        UrlEncodedFormEntity entity = new UrlEncodedFormEntity(formparams, Consts.UTF_8);
+                        if (entity != null)
+                        {
+
+                            httpPost.setEntity(entity);
+
+                            // Fire the Url
+                            CloseableHttpResponse response = httpClient.execute(httpPost);
+
+                            // verify the valid error code first
+                            int statusCode = response.getStatusLine().getStatusCode();
+                            if (statusCode != HttpStatus.OK.value())
+                            {
+                                log.info(
+                                        "Error obtaining Access Token: Http REquest failed with Status  " + statusCode);
+                                log.info("Error Details :  " + response.getEntity().toString());
+                                return new ResponseEntity<>(HttpStatus.EXPECTATION_FAILED);
+                            }
+
+                            else
+                            {
+                                // Parse the JSON response
+                                HttpEntity entityResp = response.getEntity();
+                                String apiOutput = EntityUtils.toString(entityResp);
+
+                                // Conerting to JSON
+                                ObjectMapper mapper = new ObjectMapper();
+                                JsonNode jsonNode = mapper.readTree(apiOutput);
+                                if (jsonNode != null)
+                                {
+                                    bearer = new TY_BearerToken();
+                                    // Get the access token
+                                    String accessToken = jsonNode.get("access_token").asText();
+                                    if (StringUtils.hasText(accessToken))
+                                    {
+                                        bearer.setAccessToken(accessToken);
+                                    }
+
+                                    if (StringUtils.hasText(String.valueOf(jsonNode.get("expires_in").asInt())))
+                                    {
+                                        bearer.setExpiresIn(jsonNode.get("expires_in").asInt());
+
+                                    }
+
+                                    if (StringUtils.hasText(jsonNode.get("scope").asText()))
+                                    {
+                                        bearer.setScope(jsonNode.get("scope").asText());
+                                    }
+                                }
+
+                            }
+                        }
+
+                    }
+
+                }
+
+            }
+        }
+
+        catch (Exception e)
+        {
+            log.info("Error accessing Destination  " + desNameOAuthCred);
+            log.info("Error Details :  " + e.getLocalizedMessage());
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        finally
+        {
+            httpClient.close();
+        }
+
+        return new ResponseEntity<>(bearer, HttpStatus.OK);
+    }
+
     @GetMapping("/csrf-token")
     public CsrfToken getCSRFToken(HttpServletRequest request) throws IOException
     {
